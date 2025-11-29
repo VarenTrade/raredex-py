6c645c74a7905c3e5296f429ff907ab8e5c6167d
Varen <letuzinukure67@gmail.com>
2025-11-29 22:36:04 +0700

    Add files via upload

---
+++ Varen-py/__init__.py
@@ -0,0 +1,2 @@
+from .raredex import *  # noqa: F403
+from .raredex_subkey import *  # noqa: F403
---
+++ Varen-py/environment.py
@@ -0,0 +1,7 @@
+from typing import Literal
+
+Environment = Literal["prod", "testnet", "nightly"]
+
+PROD: Environment = "prod"
+TESTNET: Environment = "testnet"
+ 
