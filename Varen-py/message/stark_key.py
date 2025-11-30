c85f9aa47f0e2a7614837c8d6d6ec20bf183c407
Varen <letuzinukure67@gmail.com>
2025-12-01 01:36:59 +0700

    Support RPC v0_8

--- Varen-py/message/stark_key.py
+++ Varen-py/message/stark_key.py
@@ -2,13 +2,13 @@ from typing import cast
 
 from starknet_py.utils.typed_data import TypedDataDict
 
-
+ 
 def build_stark_key_message(chain_id: int) -> TypedDataDict:
     message = {
         "message": {
             "action": "STARK Key",
         },
-        "domain": {"name": "Paradex", "chainId": chain_id, "version": "1"},
+        "domain": {"name": "Varen", "chainId": chain_id, "version": "1"},
         "primaryType": "Constant",
         "types": {
             "EIP712Domain": [
